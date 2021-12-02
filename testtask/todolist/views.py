dd796ef01b4272f97eadc7d45b6bc3d1d2b93c48
Bekarys Magauiya <maagauiya@MacBook-Air-Bekarys.local>
2021-12-03 03:56:26 +0600

    yeah boy done at 4

--- testtask/todolist/views.py
+++ testtask/todolist/views.py
@@ -9,7 +9,30 @@ from django.contrib.auth.decorators import login_required
 import json 
 from django.forms.models import model_to_dict
 from .models import*
+from django.core import serializers
 # Create your views here.
 
 def index(request):
-    return render(request,'todolist/main.html')
+    tasks= Todolist.objects.all()
+    context={
+        "tasks" : tasks,
+    }
+    if request.method=='POST':
+        if request.POST.get('create'):
+            taskname = request.POST['taskname']
+            task = Todolist()
+            task.task_text=taskname
+            task.status=2
+            task.save()
+        elif request.POST.get('refresh'):
+            return render(request,'todolist/main.html',context=context)
+        elif request.POST.get('update'):
+            t_upd = Todolist.objects.get(id=request.POST["prikey"])
+            t_upd.task_text = request.POST["old_task"] 
+            t_upd.status= request.POST["option1"]
+            t_upd.save()
+
+        elif request.POST.get('delete'):
+            t_del = Todolist.objects.get(id=request.POST["prikey"])
+            t_del.delete()
+    return render(request,'todolist/main.html',context=context)
