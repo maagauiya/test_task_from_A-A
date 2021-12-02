bc0058dccbe6a180697d0436308dc6b4d7862686
Bekarys Magauiya <maagauiya@MacBook-Air-Bekarys.local>
2021-12-03 03:58:27 +0600

    removed modules from my old file

--- testtask/todolist/views.py
+++ testtask/todolist/views.py
@@ -1,15 +1,5 @@
-import json
-from django.core import serializers
-from django.http.response import Http404, HttpResponseNotFound, HttpResponseRedirect
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
-from django.views.decorators.csrf import csrf_protect
-from django.contrib.auth.decorators import login_required
-import json 
-from django.forms.models import model_to_dict
+from django.shortcuts import render
 from .models import*
-from django.core import serializers
 # Create your views here.
 
 def index(request):
